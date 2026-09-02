ece42eb004d79fb682847fdee15e6e5625f8705a
Thana <chua.thanaseth@gmail.com>
2026-09-03 02:27:49 +0700

    test: update cloud evidence asset expectations

--- tests/test_project_structure.py
+++ tests/test_project_structure.py
@@ -30,55 +30,16 @@ def test_required_project_files_exist():
 
 def test_architecture_assets_exist():
     required_assets = [
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "00_cloud_data_platform_architecture.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "01_s3_full_gold_marts.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "02_athena_batch_query.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "03_athena_streaming_validation.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "04_redshift_batch_landing.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "05_redshift_batch_analytics.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "06_redshift_streaming_validation.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "07_redshift_runtime_metadata.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "08_cloud_tests_and_lint.png",
-
-        ROOT_DIR
-        / "assets"
-        / "redshift"
-        / "09_cloud_ci_success.png",
+        ROOT_DIR / "assets" / "redshift" / "00_cloud_data_platform_architecture.png",
+        ROOT_DIR / "assets" / "redshift" / "01_s3_batch_gold.png",
+        ROOT_DIR / "assets" / "redshift" / "02_s3_streaming_latest_pointer.png",
+        ROOT_DIR / "assets" / "redshift" / "03_athena_batch_cross_layer_validation.png",
+        ROOT_DIR / "assets" / "redshift" / "04_streaming_cross_layer_validation.png",
+        ROOT_DIR / "assets" / "redshift" / "05_redshift_batch_validation.png",
+        ROOT_DIR / "assets" / "redshift" / "06_redshift_streaming_validation.png",
+        ROOT_DIR / "assets" / "redshift" / "07_cloud_tests_and_lint.png",
+        ROOT_DIR / "assets" / "redshift" / "08_cloud_ci_success.png",
+        ROOT_DIR / "assets" / "redshift" / "09_latest_pointer.png",
     ]
 
     for asset_path in required_assets:
